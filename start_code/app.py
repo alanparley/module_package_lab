203059f09d208160fc9fcc5f806f8fcccdc71c6d
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-10 16:12:37 +0100

    intitial module changes made as per task

--- start_code/app.py
+++ start_code/app.py
@@ -1,13 +1,5 @@
-def print_menu():
-    print("Options:")
-    print("1: Display All Tasks")
-    print("2: Get Uncompleted Tasks")
-    print("3: Get Completed Tasks")
-    print("4: Mark Task as Complete")
-    print("5: Get Tasks Which Take Longer Than a Given Time")
-    print("6: Find Task by Description")
-    print("7: Add a new Task to list")
-    print("Q or q: Quit")
+from task_list import *
+from output import print_menu, print_list, print_task, print_task_descriptions
 
 while (True):
     print_menu()
